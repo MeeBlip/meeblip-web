e6f9dc16f5ddfa4501bd115f1d17e675106ab007
Ben Schmaus <benjamin.schmaus@gmail.com>
2017-02-27 17:30:38 -0500

    initial commit

---
+++ triode/patches.js
@@ -0,0 +1,18 @@
+var patches = [
+{
+	name: "punchy",
+	href: "./?aDecay=80&fDecay=100&sustain=0&fCut=100&fRes=15&wave=0&detune=20&octave=127&pwmSweep=127&lRate=77&lDepth=12&lDest=0&glide=0&fAccnt=0&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+	description: "quick and thick"
+},
+{
+  name: "little bug",
+  href: "./?aDecay=81&fDecay=85&sustain=0&fCut=82&fRes=77&wave=31&detune=64&octave=0&pwmSweep=127&lRate=72&lDepth=24&lDest=0&glide=0&fAccnt=118&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+  description: "light sound with a little wiggle"
+},
+{
+  name: "saucers",
+  href: "./?aDecay=81&fDecay=85&sustain=127&fCut=82&fRes=77&wave=31&detune=64&octave=127&pwmSweep=127&lRate=72&lDepth=24&lDest=0&glide=0&fAccnt=118&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+  description: "sci-fi vibe"
+
+}
+];
