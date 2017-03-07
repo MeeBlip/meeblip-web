eee2f4a0c2a5b787eb29ec1e7643373ee8922074
Ben Schmaus <benjamin.schmaus@gmail.com>
2017-03-06 20:53:59 -0500

    adds some triode patches

--- anode/patches.js
+++ anode/patches.js
@@ -2,42 +2,42 @@ var patches = [
 
 {
 	name: "squiggles",
-	href: "./?aDecay=127&fDecay=127&sustain=127&fCut=27&wave=48&detune=62&octave=127&pwmSweep=127&lRate=83&lDepth=127&lDest=0&glide=64&vcfEnvAmount=88&lRand=127&lNoteRetrig=127&oscBWave=127&aAttck=0&fAttck=0",
+	href: "?aDecay=127&fDecay=127&sustain=127&fCut=27&wave=48&detune=62&octave=127&pwmSweep=127&lRate=83&lDepth=127&lDest=0&glide=64&vcfEnvAmount=88&lRand=127&lNoteRetrig=127&oscBWave=127&aAttck=0&fAttck=0",
 	description: "bleeps with lfo random"
 },
 {
 	name: "chunked",
-	href: "./?aDecay=99&fDecay=57&sustain=127&fCut=92&wave=16&detune=107&octave=0&pwmSweep=0&lRate=30&lDepth=0&lDest=0&glide=35&vcfEnvAmount=95&lRand=0&lNoteRetrig=127&oscBWave=0&aAttck=50&fAttck=8",
+	href: "?aDecay=99&fDecay=57&sustain=127&fCut=92&wave=16&detune=107&octave=0&pwmSweep=0&lRate=30&lDepth=0&lDest=0&glide=35&vcfEnvAmount=95&lRand=0&lNoteRetrig=127&oscBWave=0&aAttck=50&fAttck=8",
 	description: "power chord riffing"
 },
 {
 	name: "buzz",
-	href: "./?aDecay=64&fDecay=84&sustain=127&fCut=95&wave=32&detune=64&octave=127&pwmSweep=127&lRate=80&lDepth=34&lDest=127&glide=96&vcfEnvAmount=124&lRand=0&lNoteRetrig=127&oscBWave=127&aAttck=0&fAttck=0",
+	href: "?aDecay=64&fDecay=84&sustain=127&fCut=95&wave=32&detune=64&octave=127&pwmSweep=127&lRate=80&lDepth=34&lDest=127&glide=96&vcfEnvAmount=124&lRand=0&lNoteRetrig=127&oscBWave=127&aAttck=0&fAttck=0",
 	description: "buzz tone with glide"	
 },
 {
 	name: "phlead",
-	href: "./?aDecay=100&fDecay=101&sustain=127&fCut=89&wave=0&detune=69&octave=0&pwmSweep=0&lRate=58&lDepth=16&lDest=127&glide=30&vcfEnvAmount=64&lRand=0&lNoteRetrig=127&oscBWave=0&aAttck=0&fAttck=0",
+	href: "?aDecay=100&fDecay=101&sustain=127&fCut=89&wave=0&detune=69&octave=0&pwmSweep=0&lRate=58&lDepth=16&lDest=127&glide=30&vcfEnvAmount=64&lRand=0&lNoteRetrig=127&oscBWave=0&aAttck=0&fAttck=0",
 	description: "big pwm sound"
 },
 {
 	name: "slooow fall",
-	href: "./?aDecay=74&fDecay=127&sustain=127&fCut=100&wave=112&detune=64&octave=0&pwmSweep=127&lRate=1&lDepth=127&lDest=0&glide=30&vcfEnvAmount=127&lRand=0&lNoteRetrig=0&oscBWave=0&aAttck=0&fAttck=43",
+	href: "?aDecay=74&fDecay=127&sustain=127&fCut=100&wave=112&detune=64&octave=0&pwmSweep=127&lRate=1&lDepth=127&lDest=0&glide=30&vcfEnvAmount=127&lRand=0&lNoteRetrig=0&oscBWave=0&aAttck=0&fAttck=43",
 	description: "it's a long way down"
 },
 {
 	name: "ah ah ah ah ah",
-	href: "./?aDecay=99&fDecay=116&sustain=127&fCut=65&wave=0&detune=28&octave=127&pwmSweep=0&lRate=10&lDepth=100&lDest=127&glide=0&vcfEnvAmount=102&lRand=0&lNoteRetrig=0&oscBWave=0&aAttck=82&fAttck=0",
-	description: "reminds me of harmonized guitar part in black dog"
+	href: "?aDecay=99&fDecay=116&sustain=127&fCut=65&wave=0&detune=28&octave=127&pwmSweep=0&lRate=10&lDepth=100&lDest=127&glide=0&vcfEnvAmount=102&lRand=0&lNoteRetrig=0&oscBWave=0&aAttck=82&fAttck=0",
+	description: "reminds me of harmonized guitar part in zep's black dog"
 },
 {
 	name: "harpsiblip",
-	href: "./?aDecay=90&fDecay=24&sustain=0&fCut=75&wave=112&detune=64&octave=127&pwmSweep=0&lRate=0&lDepth=0&lDest=127&glide=0&vcfEnvAmount=44&lRand=0&lNoteRetrig=0&oscBWave=0&aAttck=0&fAttck=65",
+	href: "?aDecay=90&fDecay=24&sustain=0&fCut=75&wave=112&detune=64&octave=127&pwmSweep=0&lRate=0&lDepth=0&lDest=127&glide=0&vcfEnvAmount=44&lRand=0&lNoteRetrig=0&oscBWave=0&aAttck=0&fAttck=65",
 	description: "kind of a harpsichord-ish thing"
 },
 {
 	name: "shorty",
-	href: "./?aDecay=80&fDecay=94&sustain=0&fCut=40&wave=48&detune=64&octave=127&pwmSweep=127&lRate=23&lDepth=16&lDest=127&glide=0&vcfEnvAmount=99&lRand=0&lNoteRetrig=127&oscBWave=0&aAttck=0&fAttck=65",
+	href: "?aDecay=80&fDecay=94&sustain=0&fCut=40&wave=48&detune=64&octave=127&pwmSweep=127&lRate=23&lDepth=16&lDest=127&glide=0&vcfEnvAmount=99&lRand=0&lNoteRetrig=127&oscBWave=0&aAttck=0&fAttck=65",
 	description: "softer sound with quick release"
 }
 
--- triode/patches.js
+++ triode/patches.js
@@ -1,18 +1,38 @@
 var patches = [
 {
 	name: "punchy",
-	href: "./?aDecay=80&fDecay=100&sustain=0&fCut=100&fRes=15&wave=0&detune=20&octave=127&pwmSweep=127&lRate=77&lDepth=12&lDest=0&glide=0&fAccnt=0&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+	href: "?aDecay=80&fDecay=100&sustain=0&fCut=100&fRes=15&wave=0&detune=20&octave=127&pwmSweep=127&lRate=77&lDepth=12&lDest=0&glide=0&fAccnt=0&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
 	description: "quick and thick"
 },
 {
   name: "little bug",
-  href: "./?aDecay=81&fDecay=85&sustain=0&fCut=82&fRes=77&wave=31&detune=64&octave=0&pwmSweep=127&lRate=72&lDepth=24&lDest=0&glide=0&fAccnt=118&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
-  description: "light sound with a little wiggle"
+  href: "?aDecay=81&fDecay=85&sustain=0&fCut=82&fRes=77&wave=31&detune=64&octave=0&pwmSweep=127&lRate=72&lDepth=24&lDest=0&glide=0&fAccnt=118&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+  description: "light sound with just a little wiggle"
 },
 {
   name: "saucers",
-  href: "./?aDecay=81&fDecay=85&sustain=127&fCut=82&fRes=77&wave=31&detune=64&octave=127&pwmSweep=127&lRate=72&lDepth=24&lDest=0&glide=0&fAccnt=118&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+  href: "?aDecay=81&fDecay=85&sustain=127&fCut=82&fRes=77&wave=31&detune=64&octave=127&pwmSweep=127&lRate=72&lDepth=24&lDest=0&glide=0&fAccnt=118&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
   description: "sci-fi vibe"
 
+},
+{
+  name: "racecar",
+  href: "?aDecay=90&fDecay=64&sustain=127&fCut=20&fRes=60&wave=78&detune=90&octave=127&pwmSweep=0&lRate=70&lDepth=42&lDest=0&glide=0&fAccnt=0&vcfEnvAmount=85&lRand=127&lNoteRetrig=127&oscPW=0&aAttck=27&fAttck=0",
+  description: "shifting gears with lfo rand"
+},
+{
+  name: "rezwhoa",
+  href: "?aDecay=44&fDecay=94&sustain=127&fCut=28&fRes=71&wave=0&detune=71&octave=0&pwmSweep=64&lRate=54&lDepth=90&lDest=127&glide=0&fAccnt=0&vcfEnvAmount=96&lRand=0&lNoteRetrig=127&oscPW=52&aAttck=32&fAttck=0",
+  description: "resonant wah sound"
+},
+{
+  name: "zapped",
+  href: "?aDecay=92&fDecay=127&sustain=0&fCut=34&fRes=17&wave=0&detune=29&octave=127&pwmSweep=0&lRate=92&lDepth=110&lDest=0&glide=0&fAccnt=0&vcfEnvAmount=82&lRand=0&lNoteRetrig=127&oscPW=0&aAttck=92&fAttck=0",
+  description: "the martians are here"
+},
+{
+  name: "basic saw",
+  href: "?aDecay=77&fDecay=127&sustain=0&fCut=56&fRes=0&wave=0&detune=71&octave=127&pwmSweep=0&lRate=25&lDepth=10&lDest=127&glide=0&fAccnt=0&vcfEnvAmount=57&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+  description: "your basic saw bass"
 }
 ];
