8b89c89e383eb71e14b762f1d59f9f0282343148
Ben Schmaus <benjamin.schmaus@gmail.com>
2017-03-08 20:30:35 -0500

    start sequencer withouting waiting delay interval and add a few more patches

--- triode/patches.js
+++ triode/patches.js
@@ -34,5 +34,20 @@ var patches = [
   name: "basic saw",
   href: "?aDecay=77&fDecay=127&sustain=0&fCut=56&fRes=0&wave=0&detune=71&octave=127&pwmSweep=0&lRate=25&lDepth=10&lDest=127&glide=0&fAccnt=0&vcfEnvAmount=57&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
   description: "your basic saw bass"
+},
+{
+  name: "multi-percus",
+  href: "?aDecay=42&fDecay=26&sustain=0&fCut=52&fRes=67&wave=0&detune=0&octave=127&pwmSweep=64&lRate=25&lDepth=0&lDest=127&glide=0&fAccnt=118&vcfEnvAmount=127&lRand=0&lNoteRetrig=0&oscPW=64&aAttck=0&fAttck=0",
+  description: "a multi-purpose percussion sound"
+},
+{
+  name: "init",
+  href: "?aDecay=0&fDecay=0&sustain=127&fCut=64&fRes=64&wave=0&detune=64&octave=127&pwmSweep=64&lRate=0&lDepth=7&lDest=127&glide=0&fAccnt=0&vcfEnvAmount=100&lRand=0&lNoteRetrig=127&oscPW=64&aAttck=0&fAttck=0",
+  description: "pwm wave with sub osc"
+},
+{
+  name: "squiggle",
+  href: "?aDecay=92&fDecay=84&sustain=127&fCut=50&fRes=17&wave=0&detune=64&octave=0&pwmSweep=64&lRate=90&lDepth=110&lDest=0&glide=0&fAccnt=0&vcfEnvAmount=127&lRand=127&lNoteRetrig=0&oscPW=51&aAttck=14&fAttck=15",
+  description: "frenetic bleeps"
 }
 ];
